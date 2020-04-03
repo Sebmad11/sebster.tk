a40dba9bf3b066e2847867f11d8af810e8f09282
Sebmad11 <61243344+Sebmad11@users.noreply.github.com>
2020-04-03 18:15:59 +0200

    Delete odbierz.php

--- php/odbierz.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if (count($_POST))
-	echo 'Witaj '. $_POST['imie'];
-else  echo '<form action="" method="POST">
-<input type="text" id="imie" name="imie">
-<input type="submit" value="Wyślij">
-</form>'; ?>
